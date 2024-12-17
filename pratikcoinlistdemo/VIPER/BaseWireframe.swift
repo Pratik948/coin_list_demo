184e972bacf057d74d229afefd16405b8bf024f0
pratikathubilo <jamariyapratik@gmail.com>
2024-12-17 21:14:52 +0530

    Initial VIPER Setup

---
+++ pratikcoinlistdemo/VIPER/BaseWireframe.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+public protocol WireframeInterface: AnyObject {
+    func popFromNavigationController(animated: Bool)
+    func dismiss(animated: Bool)
+}
+
+open class Wireframe {
+
+    fileprivate var _viewController: UIViewController
+
+    public init(viewController: UIViewController) {
+        _viewController = viewController
+    }
+    public var viewController: UIViewController {
+        return _viewController
+    }
+
+    var navigationController: UINavigationController? {
+        return viewController.navigationController
+    }
+}
+
+extension Wireframe: WireframeInterface {
+    public func popFromNavigationController(animated: Bool) {
+        let _ = navigationController?.popViewController(animated: animated)
+    }
+
+    public func dismiss(animated: Bool) {
+        navigationController?.dismiss(animated: animated)
+    }
+}
+
+public extension UIViewController {
+
+    func presentWireframe(_ wireframe: Wireframe, animated: Bool = true, completion: (() -> Void)? = nil) {
+        present(wireframe.viewController, animated: animated, completion: completion)
+    }
+
+}
+
+public extension UINavigationController {
+
+    func pushWireframe(_ wireframe: Wireframe, animated: Bool = true) {
+        self.pushViewController(wireframe.viewController, animated: animated)
+    }
+
+    func setRootWireframe(_ wireframe: Wireframe, animated: Bool = true) {
+        self.setViewControllers([wireframe.viewController], animated: animated)
+    }
+}
+
+public extension UITabBarController {
+    
+    func setWireframes(_ wireframes: Wireframe..., animated: Bool = false) {
+        self.setViewControllers(wireframes.compactMap({ $0.viewController }), animated: animated)
+    }
+   
+}
---
+++ pratikcoinlistdemo/VIPER/ViewInterface.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+public protocol ViewInterface: AnyObject {
+    func showSpinner()
+    func hideSpinner()
+}
+
+public extension ViewInterface {
+
+    func showSpinner() {
+        
+    }
+
+    func hideSpinner() {
+
+    }
+}
